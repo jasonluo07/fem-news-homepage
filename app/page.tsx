b3db3e973668dbbc0adc46f4b084be25c62fcd97
Jason Luo <yulaie1012@gmail.com>
2024-07-31 11:00:47 +0800

    feat: create featured-articles section

--- app/page.tsx
+++ app/page.tsx
@@ -79,7 +79,49 @@ export default function Home() {
             </li>
           </ul>
         </section>
-        <section className="featured-articles"></section>
+        <section className="featured-articles">
+          <ul className="featured-articles__list">
+            <li className="featured-articles__item">
+              <Image
+                className="featured-articles__image"
+                src="/image-retro-pcs.jpg"
+                alt="Reviving Retro PCs"
+                width={100}
+                height={127}
+              />
+              <div className="featured-articles__content">
+                <h3 className="featured-articles__title">Reviving Retro PCs</h3>
+                <p className="featured-articles__description">What happens when old PCs are given modern upgrades?</p>
+              </div>
+            </li>
+            <li className="featured-articles__item">
+              <Image
+                className="featured-articles__image"
+                src="/image-top-laptops.jpg"
+                alt="Top 10 Laptops of 2022"
+                width={100}
+                height={127}
+              />
+              <div className="featured-articles__content">
+                <h3 className="featured-articles__title">Top 10 Laptops of 2022</h3>
+                <p className="featured-articles__description">Our best picks for various needs and budgets.</p>
+              </div>
+            </li>
+            <li className="featured-articles__item">
+              <Image
+                className="featured-articles__image"
+                src="/image-gaming-growth.jpg"
+                alt="The Growth of Gaming"
+                width={100}
+                height={127}
+              />
+              <div className="featured-articles__content">
+                <h3 className="featured-articles__title">The Growth of Gaming</h3>
+                <p className="featured-articles__description">How the pandemic has sparked fresh opportunities.</p>
+              </div>
+            </li>
+          </ul>
+        </section>
       </main>
     </>
   );
