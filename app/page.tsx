32d03d659054f160a5dc87cf9846683e8e089097
Jason Luo <yulaie1012@gmail.com>
2024-07-31 10:15:54 +0800

    feat: create hero section

--- app/page.tsx
+++ app/page.tsx
@@ -42,14 +42,18 @@ export default function Home() {
       </header>
       <main className="main">
         <section className="hero">
-
+          <Image src="/image-web-3-mobile.jpg" alt="The Bright Future of Web 3.0?" width={343} height={300} />
+          <h1 className="hero__title">The Bright Future of Web 3.0?</h1>
+          <p className="hero__description">
+            We dive into the next evolution of the web that claims to put the power of the platforms back into the hands
+            of the people. But is it really fulfilling its promise?
+          </p>
+          <a href="#" className="hero-button">
+            Read More
+          </a>
         </section>
-        <section className="latest-news">
-
-        </section>
-        <section className="featured-articles">
-
-        </section>
+        <section className="latest-news"></section>
+        <section className="featured-articles"></section>
       </main>
     </>
   );
