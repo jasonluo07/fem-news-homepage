c3418068dea30fc122483012ab2f8a1d03f315d1
Jason Luo <yulaie1012@gmail.com>
2024-07-31 10:26:54 +0800

    feat: create latest-news section

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,33 @@ export default function Home() {
             Read More
           </a>
         </section>
-        <section className="latest-news"></section>
+        <section className="latest-news">
+          <h2 className="latest-news__title">New</h2>
+          <ul className="latest-news__list">
+            <li className="latest-news__item">
+              <h3 className="latest-news__item-title">
+                <a href="#">Hydrogen VS Electric Cars</a>
+              </h3>
+              <p className="latest-news__item-description">Will hydrogen-fueled cars ever catch up to EVs?</p>
+            </li>
+            <li className="latest-news__item">
+              <h3 className="latest-news__item-title">
+                <a href="#">The Downsides of AI Artistry</a>
+              </h3>
+              <p className="latest-news__item-description">
+                What are the possible adverse effects of on-demand AI image generation?
+              </p>
+            </li>
+            <li className="latest-news__item">
+              <h3 className="latest-news__item-title">
+                <a href="#">Is VC Funding Drying Up?</a>
+              </h3>
+              <p className="latest-news__item-description">
+                Private funding by VC firms is down 50% YOY. We take a look at what that means.
+              </p>
+            </li>
+          </ul>
+        </section>
         <section className="featured-articles"></section>
       </main>
     </>
