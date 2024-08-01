38ad8f9ef9d6b4dcee0f52660ff6a80ced3f97c2
Jason Luo <yulaie1012@gmail.com>
2024-08-01 09:18:35 +0800

    chore: import all components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import Header from '@/app/components/header';
+import FeaturedArticles from './components/featured-articles';
+import Hero from './components/hero';
+import LatestNews from './components/latest-news';
 
 export default function Home() {
   return (
     <div className="mx-4 mb-20 mt-7 max-w-[1110px] md:mx-auto">
       <Header />
-      {/* <main className="main">
+      <main className="main">
         <Hero />
         <LatestNews />
         <FeaturedArticles />
-      </main> */}
+      </main>
     </div>
   );
 }
